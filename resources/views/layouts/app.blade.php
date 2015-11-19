d17678f784575d2ac2d0dc353a6ffb9315a937b6
jcaicedo <caicedo.julio@gmail.com>
2015-11-19 00:19:46 -0430

    menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,94 +29,49 @@
 
 <!--NAVBAR CON LOGIN-->
 
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#menu">
+            <span class="sr-only">GenOACAUCV</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
 
+        </button>
+        <a href="#" class="navbar-brand">GenOACAUCV</a>
 
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigarion">
-    <div class="container">
+    </div>
+    <div class="collapse navbar-collapse" id="menu">
 
-        <div class="navbar-header">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Inicio</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nosotros<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Proyecto OACA</a></li>
+                    <li><a href="#">Instituciones Piloto</a></li>
+                    <li><a href="#">OACA</a></li>
+                    <li><a href="#">Equipo Desarrollo</a></li>
 
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="#" class="navbar-brand">GenOacaUCV</a>
-        </div>
+                </ul>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Inicio</a></li>
-                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Nosotros <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Proyecto OACA</a></li>
-                        <li><a href="#">Instituciones Piloto</a></li>
-                        <li><a href="#">OACA</a></li>
-                        <li><a href="#">Equipo Desarrollo</a></li>
+            </li>
 
-                    </ul>
-                </li>
-                <li><a href="#">Generador</a></li>
-                <li><a href="#">Contacto</a></li>
-            </ul>
+        </ul>
 
-            <ul class="nav navbar-nav navbar-right">
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login
-                <span class="glyphicon glyphicon-log-in">
-                </span>
-                    </a>
-                    <div class="dropdown-menu">
-
-                        <form action="" id="formlogin" class="form container-fluid">
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                                <label for="usr">Name:</label>
-                                <input type="text" class="form-control" id="usr">
-                            </div>
-                            <div class="form-group">
-                                <label for="pwd">Password:</label>
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                                <input type="password" class="form-control" id="pwd">
-                            </div>
-                            <button class="btn btn-block" id="btnLogin" type="button">Login</button>
-
-
-                        </form>
-                        <div class="container-fluid">
-                            <!--                    <br>-->
-                            <a href="#" class="small">Forgot password?</a>
-                        </div>
-
-
-                    </div>
-
-
-
-
-                </li>
-
-
-            </ul>
-
-        </div>
 
 
     </div>
-
-
 </nav>
 
-<hr>
 
+{{--@yield('menu1')--}}
 
-<div class="container">
+{{--<div class="container">--}}
 
-@yield('content')
+{{--@yield('content')--}}
 
-</div>
+{{--</div>--}}
 
 {{--Script--}}
 
