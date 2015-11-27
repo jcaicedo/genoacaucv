0a2cdacea5e0adf5384fbc0fab85f8b7ed6ce58a
Julio Caicedo <juliocaicedo@Bees-MacBook-Pro.local>
2015-11-27 13:39:13 -0430

    agregando elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,24 @@
 
 <figure>
     <div class="hero">
-        <button type="button" class="btn btn-primary btn-lg"><h1>CREAR</h1></button>
+
+        <a class="btn btn-lg btn-primary boton_Crear_oaca" href="">CREAR OACA</a>
+
     </div>
+
 </figure>
+
+
+
+    <div class="down" ></div>
+    <div class="experience-section" id="experience" style="height: 390px; background-attachment: fixed; background-position: 50% 4px;">
+        <div class="wrapper">
+            <div>
+                <h2>The Experience</h2>
+                <p>The Krash experience is like living in a particle accelerator for people. Beyond the optimal living environment, both the houses and the programming are designed to maximize collaboration, innovation and the establishment of life-long friendships.</p>
+            </div>
+        </div>
+    </div>
 
 
 
