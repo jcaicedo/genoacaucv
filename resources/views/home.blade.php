6493cff8eed85615cb74a24dfb77a62932499b1f
jcaicedo <caicedo.julio@gmail.com>
2015-12-01 23:21:15 -0430

    agregando elementos y jumbrotron arreglado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-md-12 jumbotron">
             <div class="text-center">
-                <p>Sistema de Gestion de Objetos de Aprendizaje de Contenidos Abietos Accesibles</p>
+                <p id="p-titulo">Sistema de Gestion de Objetos de Aprendizaje de Contenidos Abietos Accesibles</p>
             </div>
         </div>
 
@@ -18,7 +18,25 @@
 
 </div>
     <div class="down" style="margin-bottom:-20px;"></div>
-    <div class="experience-section" id="flexible" style="height: 909px; background-attachment: fixed; background-position: 50% -2px;">
+
+
+    <div class="experience-section" id="flexible" style="height: 500px; background-attachment: fixed; background-position: 50% -2px;">
+
+        <div class="container-fluid" id="container-intro">
+            <div class="row">
+                <div class="col-md-7 col-md-offset-1" id="text-intro">
+                        <h2>SISGEOACA</h2>
+                    <p>El Sistema de Gestión de Objetos de Aprendizaje de Contenidos Abiertos Accesibles, SisGeOACAA es la integración
+                        de un conjunto de componentes denominados herramientas tecnológicas que permiten la creación, gestión, evaluación,
+                        almacenamiento y uso de estos recursos para enriquecer los procesos de enseñanza y aprendizaje, a nivel de educación
+                        media general, diversificada y universitaria.</p>
+
+
+                </div>
+            </div>
+        </div>
+
+
 
     </div>
 
