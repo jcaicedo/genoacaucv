319fb24d395d7c45709f0549a15eb2eed1954bb8
jcaicedo <caicedo.julio@gmail.com>
2015-12-02 00:02:03 -0430

    arreglando componenetes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,20 +17,28 @@
     </div>
 
 </div>
+
+    {{--flechita de transicion--}}
     <div class="down" style="margin-bottom:-20px;"></div>
 
-
-    <div class="experience-section" id="flexible" style="height: 500px; background-attachment: fixed; background-position: 50% -2px;">
+{{--Inicio de Introduccion--}}
+    <div class="experience-section" id="flexible" style="height: 700px; background-attachment: fixed; background-position: 50% -2px;">
 
         <div class="container-fluid" id="container-intro">
             <div class="row">
-                <div class="col-md-7 col-md-offset-1" id="text-intro">
+                <div class="col-md-5 col-sm-5 col-xs-6 col-md-offset-1" id="text-intro">
                         <h2>SISGEOACA</h2>
+                    <br>
                     <p>El Sistema de Gestión de Objetos de Aprendizaje de Contenidos Abiertos Accesibles, SisGeOACAA es la integración
                         de un conjunto de componentes denominados herramientas tecnológicas que permiten la creación, gestión, evaluación,
                         almacenamiento y uso de estos recursos para enriquecer los procesos de enseñanza y aprendizaje, a nivel de educación
                         media general, diversificada y universitaria.</p>
 
+
+                </div>
+
+                <div class="col-md-2 col-md-offset-4" style="background-color: #0000C2;height: 100%;position: relative;right: 10%">
+hoalallalaa
 
                 </div>
             </div>
@@ -40,7 +48,7 @@
 
     </div>
 
-
+{{--fin de Introduccion--}}
 
 
 @endsection
