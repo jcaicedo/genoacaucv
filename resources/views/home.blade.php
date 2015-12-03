e620da4f96f31aca1574848304993d521aab67a3
jcaicedo <caicedo.julio@gmail.com>
2015-12-02 22:36:19 -0430

    arreglada la Introduccion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,11 +22,28 @@
     <div class="down" style="margin-bottom:-20px;"></div>
 
 {{--Inicio de Introduccion--}}
-    <div class="experience-section" id="flexible" style="height: 700px; background-attachment: fixed; background-position: 50% -2px;">
 
-        <div class="container-fluid" id="container-intro">
+    <div class="container-fluid" id="Intro" >
+        <div class="experience-section" id="experience" style="height: 800px; background-attachment: fixed; background-position: 50% 1px;">
+                <div class="wrapper">
+                    <div>
+                        <h2>SISGEOACA</h2>
+                        <p>El Sistema de Gestión de Objetos de Aprendizaje de Contenidos Abiertos Accesibles, SisGeOACAA es la integración
+                            de un conjunto de componentes denominados herramientas tecnológicas que permiten la creación, gestión, evaluación,
+                            almacenamiento y uso de estos recursos para enriquecer los procesos de enseñanza y aprendizaje, a nivel de educación
+                            media general, diversificada y universitaria.</p>
+                    </div>
+                </div>
+        </div>
+
+    </div>
+
+  {{--  <div class="experience-section flexible"  style="height: 700px; background-attachment: fixed; background-position: 50% -2px;">
+
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-5 col-sm-5 col-xs-6 col-md-offset-1" id="text-intro">
+
+                <div class="col-md-5 col-sm-5 col-xs-6 col-md-offset-1" style="background-color: #0f0f0f">
                         <h2>SISGEOACA</h2>
                     <br>
                     <p>El Sistema de Gestión de Objetos de Aprendizaje de Contenidos Abiertos Accesibles, SisGeOACAA es la integración
@@ -37,6 +54,7 @@
 
                 </div>
 
+
                 <div class="col-md-2 col-md-offset-4" style="background-color: #0000C2;height: 100%;position: relative;right: 10%">
                     <img src="" alt="">
 
@@ -46,7 +64,7 @@
 
 
 
-    </div>
+    </div>--}}
 
 {{--fin de Introduccion--}}
 
